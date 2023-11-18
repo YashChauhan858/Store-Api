6575196e095bec7d2a93cd26f851d94082a35b3f
Yash Chauhan <yashchauhan858@gmail.com>
2023-11-18 13:40:21 +0530

    product schema added

---
+++ model/productsModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema(
+  {
+    productName: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: String,
+      enum: ["A", "B", "C"],
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+    },
+    feature: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("store", productSchema);
